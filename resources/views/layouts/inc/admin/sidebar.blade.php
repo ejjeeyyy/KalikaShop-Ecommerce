1e2f134f805d7131a9d0cda646061efa62627219
Earl John <mejifrancisco@tip.edu.ph>
2022-10-11 10:44:43 +0800

    sidebar dashboard href

--- resources/views/layouts/inc/admin/sidebar.blade.php
+++ resources/views/layouts/inc/admin/sidebar.blade.php
@@ -3,7 +3,7 @@
 
           {{-- Dashboard --}}
           <li class="nav-item">
-            <a class="nav-link" href="index.html">
+            <a class="nav-link" href="{{ url('admin/dashboard') }}">
               <i class="mdi mdi-home menu-icon"></i>
               <span class="menu-title">Dashboard</span>
             </a>
