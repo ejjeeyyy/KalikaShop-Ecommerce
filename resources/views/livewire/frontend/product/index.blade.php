33237ec2b38c77a35a2d922003a67d71725fe3e0
Earl John <mejifrancisco@tip.edu.ph>
2022-10-14 23:01:18 +0800

    Part 23 Filter products by BRANDS in Livewire

--- resources/views/livewire/frontend/product/index.blade.php
+++ resources/views/livewire/frontend/product/index.blade.php
@@ -1,44 +1,65 @@
 <div>
-    {{-- Stop trying to control. --}}
     <div class="row">
-    @forelse ($products as $productItem)
-                
-           
-            <div class="col-md-3">
-                    <div class="product-card">
-                        <div class="product-card-img">
-                            @if ($productItem->quantity > 0)
-                            <label class="stock bg-success">In Stock</label>
-                            @else
-                            <label class="stock bg-success">Out of Stock</label>
-                            @endif
-                            
-                            @if ($productItem->productImages->count() > 0)
-                            <a href="{{ url('/collections/'.$productItem->category->slug.'/'.$productItem->slug) }}">
-                            <img src="{{ asset($productItem->productImages[0]->image) }}" alt="{{ $productItem->name }}">
-                            </a> 
-                            @endif
-                           
-                        </div>
-                        <div class="product-card-body">
-                            <p class="product-brand">{{ $productItem->brand }}</p>
-                            <h5 class="product-name">
-                               <a href="{{ url('/collections/'.$productItem->category->slug.'/'.$productItem->slug) }}">
-                                    {{ $productItem->name }} 
-                               </a>
-                            </h5>
-                            <div>
-                                <span class="selling-price">₱ {{ $productItem->selling_price }}</span>
-                                <span class="original-price">₱ {{ $productItem->original_price }}</span>
-                            </div>
-                         
-                        </div>
-                    </div>
+        <div class="col-md-3">
+            @if ($category->brands)
+
+            <div class="card">
+                <div class="card-header"><h4>Brands</h4></div>
+                <div class="card-body">
+                    @foreach ($category->brands as $brandItem)
+                    <label class="d-block">
+                        <input type="checkbox" wire:model="brandInputs" value="{{ $brandItem->name }}" /> {{ $brandItem->name }}
+                    </label>
+                    @endforeach
+                </div>
             </div>
-        @empty
-        <div class="col-md-12">
-            <h5>No Products Available for {{ $category->name }}</h5>
+            @endif
         </div>
-        @endforelse
+        <div class="col-md-9">
+
+
+            {{-- Stop trying to control. --}}
+            <div class="row">
+            @forelse ($products as $productItem)
+                        
+                
+                    <div class="col-md-4">
+                            <div class="product-card">
+                                <div class="product-card-img">
+                                    @if ($productItem->quantity > 0)
+                                    <label class="stock bg-success">In Stock</label>
+                                    @else
+                                    <label class="stock bg-success">Out of Stock</label>
+                                    @endif
+                                    
+                                    @if ($productItem->productImages->count() > 0)
+                                    <a href="{{ url('/collections/'.$productItem->category->slug.'/'.$productItem->slug) }}">
+                                    <img src="{{ asset($productItem->productImages[0]->image) }}" alt="{{ $productItem->name }}">
+                                    </a> 
+                                    @endif
+                                
+                                </div>
+                                <div class="product-card-body">
+                                    <p class="product-brand">{{ $productItem->brand }}</p>
+                                    <h5 class="product-name">
+                                    <a href="{{ url('/collections/'.$productItem->category->slug.'/'.$productItem->slug) }}">
+                                            {{ $productItem->name }} 
+                                    </a>
+                                    </h5>
+                                    <div>
+                                        <span class="selling-price">₱ {{ $productItem->selling_price }}</span>
+                                        <span class="original-price">₱ {{ $productItem->original_price }}</span>
+                                    </div>
+                                
+                                </div>
+                            </div>
+                    </div>
+                @empty
+                <div class="col-md-12">
+                    <h5>No Products Available for {{ $category->name }}</h5>
+                </div>
+                @endforelse
+            </div>
+        </div>
     </div>
 </div>
