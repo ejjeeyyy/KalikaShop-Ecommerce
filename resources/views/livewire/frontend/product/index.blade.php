b1414db437b0c097f2a1b60e380d4a16c4498f8f
Joaorskyyy <mjgsales@tip.edu.ph>
2022-10-15 15:02:32 +0800

    Part 26: How to check Product Stock Availability using Livewire Laravel

--- resources/views/livewire/frontend/product/index.blade.php
+++ resources/views/livewire/frontend/product/index.blade.php
@@ -4,7 +4,7 @@
             @if ($category->brands)
 
             <div class="card">
-                <div class="card-header"><h4>Brands</h4></div>
+            <div class="card-header"><h4>Brands</h4></div>
                 <div class="card-body">
                     @foreach ($category->brands as $brandItem)
                     <label class="d-block">
@@ -14,6 +14,19 @@
                 </div>
             </div>
             @endif
+
+            <div class="card mt-3">
+            <div class="card-header"><h4>Price</h4></div>
+                <div class="card-body">
+                    <label class="d-block">
+                        <input type="radio" name="priceSort" wire:model="priceInput" value="high-to-low" /> High to Low
+                    </label>
+                    <label class="d-block">
+                        <input type="radio" name="priceSort" wire:model="priceInput" value="low-to-high" /> Low to High
+                    </label>
+                </div>
+            </div>
+
         </div>
         <div class="col-md-9">
 
