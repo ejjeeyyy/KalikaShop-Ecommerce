8d3de42fefcc91a3d00dd9e87dad969f6f8523ce
RavenTamayo <108288626+RavenTamayo@users.noreply.github.com>
2022-10-16 13:17:47 +0800

    Part 27: Add Product to Wishlist

--- app/Http/Livewire/Frontend/Product/View.php
+++ app/Http/Livewire/Frontend/Product/View.php
@@ -3,10 +3,37 @@
 namespace App\Http\Livewire\Frontend\Product;
 
 use Livewire\Component;
+use App\Models\Wishlist;
+use Illuminate\Support\Facades\Auth;
 
 class View extends Component
 {
     public $category, $product, $prodColorSelectedQuantity;
+
+    public function addToWishList($productId)
+    {
+        if(Auth::check())
+        {
+            if(Wishlist::where('user_id',auth()->user()->id)->where('product_id',$productId)->exists())
+            {
+                session()->flash('message','Product is already in your wishlist');
+            return false;
+            }
+            else{
+                Wishlist::create([
+                    'user_id' => auth()->user()->id,
+                    'product_id' => $productId
+    
+                ]);
+                session()->flash('message','Product added to your wishlist successfully');
+            }
+        }
+        else
+        {
+            session()->flash('message','Please login to continue');
+            return false;
+        }
+    }
 
     public function colorSelected($productColorId)
     {
