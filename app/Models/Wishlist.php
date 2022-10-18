a0054676194a52d40fbfc7a8f1996dea44c8cff5
Earl John <mejifrancisco@tip.edu.ph>
2022-10-18 11:29:07 +0800

    Part 29 Display Wishlist Items

--- app/Models/Wishlist.php
+++ app/Models/Wishlist.php
@@ -3,7 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Product;
 
 class Wishlist extends Model
 {
@@ -15,4 +17,14 @@ class Wishlist extends Model
         'user_id',
         'product_id'
     ];
+
+    /** 
+    * Get the user that owns the Wishlist
+    *
+    * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+    */
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'product_id', 'id');
+    }
 }
