ca468c68392629a3bae591df007b866bd006104b
Joaorskyyy <mjgsales@tip.edu.ph>
2022-10-21 02:18:58 +0800

    Part 38: Display cart items Total Price

--- app/Http/Livewire/Frontend/Cart/CartShow.php
+++ app/Http/Livewire/Frontend/Cart/CartShow.php
@@ -7,7 +7,127 @@ use Livewire\Component;
 
 class CartShow extends Component
 {
-    public $cart;
+    public $cart, $totalPrice = 0;
+
+    public function decrementQuantity(int $cartId)
+    {
+        $cartData = Cart::where('id',$cartId)->where('user_id', auth()->user()->id)->first();
+        if($cartData)
+        {
+            if($cartData->productColor()->where('id',$cartData->product_color_id)->exists()){
+
+                $productColor = $cartData->productColor()-where('id',$cartData->product_color_id)->first();
+                if($productColor->quantity > $cartData->quantity){
+                    $cartData->decrement('quantity');
+                    $this->dispatchBrowserEvent('message', [
+                    'text' => 'Quantity Updated',
+                    'type' => 'info',
+                    'status' => 200
+                ]);
+                }else{
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => 'Only '.$productColor->quantity.'Quantity Available',
+                        'type' => 'info',
+                        'status' => 200
+                    ]);
+                }
+            }else{
+                if($cartData->product->quantity > $cartData->quantity)
+                {
+                    $cartData->decrement('quantity');
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => 'Quantity Updated',
+                        'type' => 'info',
+                        'status' => 200
+                    ]);
+                }else{
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => 'Only '.$cartData->product->quantity.'Quantity Available',
+                        'type' => 'info',
+                        'status' => 200
+                    ]);
+                }
+            }
+    
+        }
+        else{
+            $this->dispatchBrowserEvent('message', [
+                'text' => 'Something Went Wrong!',
+                'type' => 'error',
+                'status' => 404
+            ]);
+        }
+    }
+
+    public function incrementQuantity(int $cartId)
+    {
+        $cartData = Cart::where('id',$cartId)->where('user_id', auth()->user()->id)->first();
+        if($cartData)
+        {
+            if($cartData->productColor()-where('id',$cartData->product_color_id)->exists()){
+                $productColor = $cartData->productColor()-where('id',$cartData->product_color_id)->first();
+                if($productColor->quantity > $cartData->quantity){
+                    $cartData->increment('quantity');
+                    $this->dispatchBrowserEvent('message', [
+                    'text' => 'Quantity Updated',
+                    'type' => 'info',
+                    'status' => 200
+                ]);
+                }else{
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => 'Only '.$productColor->quantity.'Quantity Available',
+                        'type' => 'info',
+                        'status' => 200
+                    ]);
+                }
+            }else{
+                if($cartData->product->quantity > $cartData->quantity)
+                {
+                    $cartData->increment('quantity');
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => 'Quantity Updated',
+                        'type' => 'info',
+                        'status' => 200
+                    ]);
+                }else{
+                    $this->dispatchBrowserEvent('message', [
+                        'text' => 'Only '.$cartData->product->quantity.'Quantity Available',
+                        'type' => 'info',
+                        'status' => 200
+                    ]);
+                }
+            }
+    
+        }
+        else{
+            $this->dispatchBrowserEvent('message', [
+                'text' => 'Something Went Wrong!',
+                'type' => 'error',
+                'status' => 404
+            ]);
+        }
+        
+    }
+
+    public function removeCartItem(int $cartId)
+    {
+        $cartRemoveData = Cart::where('user_id',auth()->user()->id)->where('id',$cartId)->first();
+        if($cartRemoveData){
+            $cartRemoveData->delete();
+            $this->emit('CartAddedUpdated');
+            $this->dispatchBrowserEvent('message', [
+                'text' => 'Cart Item Removed Successfully',
+                'type' => 'info',
+                'status' => 200
+            ]);
+        }else{
+            $this->dispatchBrowserEvent('message', [
+                'text' => 'Something Went Wrong!',
+                'type' => 'error',
+                'status' => 500
+            ]);
+        }
+    }
     
     public function render()
     {
