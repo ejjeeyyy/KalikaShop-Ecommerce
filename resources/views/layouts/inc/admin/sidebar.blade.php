e65066a531aaebb44a9bbd1cd4cc14eafc977f40
Earl John <mejifrancisco@tip.edu.ph>
2022-10-30 17:26:58 +0800

    Part 56: Display analytics in admin dashboard | Display Total Users, Total Orders

--- resources/views/layouts/inc/admin/sidebar.blade.php
+++ resources/views/layouts/inc/admin/sidebar.blade.php
@@ -93,17 +93,17 @@
 
           <li class="nav-item">
             <a class="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-              <i class="mdi mdi-account menu-icon"></i>
-              <span class="menu-title">User Pages</span>
+              <i class="bi bi-person-plus-fill menu-icon"></i>
+              <span class="menu-title">User Management</span>
               <i class="menu-arrow"></i>
             </a>
             <div class="collapse" id="auth">
               <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Login </a></li>
-                <li class="nav-item"> <a class="nav-link" href="pages/samples/login-2.html"> Login 2 </a></li>
-                <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html"> Register </a></li>
+                <li class="nav-item"> <a class="nav-link" href="{{ url('admin/users/create') }}"> Add User </a></li>
+                <li class="nav-item"> <a class="nav-link" href="{{ url('admin/users') }}"> View Users </a></li>
+                {{-- <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html"> Register </a></li>
                 <li class="nav-item"> <a class="nav-link" href="pages/samples/register-2.html"> Register 2 </a></li>
-                <li class="nav-item"> <a class="nav-link" href="pages/samples/lock-screen.html"> Lockscreen </a></li>
+                <li class="nav-item"> <a class="nav-link" href="pages/samples/lock-screen.html"> Lockscreen </a></li> --}}
               </ul>
             </div>
           </li>
