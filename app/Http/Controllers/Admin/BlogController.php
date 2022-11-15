7adbd034e928afb2180adf3b4036b0f38c7f109f
Earl John <mejifrancisco@tip.edu.ph>
2022-11-15 20:46:25 +0800

    Blog edit function

--- app/Http/Controllers/Admin/BlogController.php
+++ app/Http/Controllers/Admin/BlogController.php
@@ -29,7 +29,6 @@ class BlogController extends Controller
         $blog->title = $validateData['title'];
         $blog->category = $validateData['category'];
         // $blog->tags = $validateData['tags'];
-        // $blog->slug = Str::slug($validateData['slug']);
         $blog->description = $validateData['description'];
        
         $uploadPath = 'uploads/blog/';
@@ -43,12 +42,7 @@ class BlogController extends Controller
             $blog->image = $uploadPath.$filename;
         }
 
-        
-        // $blog->meta_title = $validateData['meta_title'];
-        // $blog->meta_keyword = $validateData['meta_keyword'];
-        // $blog->meta_description = $validateData['meta_description'];
-    
-        // $blog->status = $request->status == true ? '1':'0';
+
         $blog->save();
 
         return redirect('admin/blog')->with('message','Blog Added Successfully!');
@@ -71,38 +65,23 @@ class BlogController extends Controller
         $blog->description = $validateData['description'];
        
 
-        // $uploadPath = 'uploads/blog/';
-        // if($request->hasFile('image')){
-
-        //     $path ='uploads/blog/'.$blog->image;
-        //     if(File::exists($path)){
-        //         File::delete($path);
-        //     }
-
-        //     $file = $request->file('image');
-
-        //     $ext = $file->getClientOriginalExtension();
-        //     $filename = time().'.'.$ext;
-
-        //     $file->move('uploads/blog/',$filename);
-        //     $blog->image = $uploadPath.$filename;
-        // }
-
+        $uploadPath = 'uploads/blog/';
         if($request->hasFile('image')){
-            $uploadPath = 'uploads/blog/';
-            $i= 1;
-            foreach($request->file('image') as $imageFile){
-                $extention = $imageFile->getClientOriginalExtension();
-                $filename = time().$i++.'.'.$extention;
-                $imageFile->move($uploadPath,$filename);
-                $finalImagePathName = $uploadPath.$filename;
 
-                $blog->blogImages()->create([
-                    'blog_id' => $blog->id,
-                    'image' => $finalImagePathName,
-                ]);
+            $path ='uploads/blog/'.$blog->image;
+            if(File::exists($path)){
+                File::delete($path);
             }
+
+            $file = $request->file('image');
+
+            $ext = $file->getClientOriginalExtension();
+            $filename = time().'.'.$ext;
+
+            $file->move('uploads/blog/',$filename);
+            $blog->image = $uploadPath.$filename;
         }
+
         
         $blog->update();
 
