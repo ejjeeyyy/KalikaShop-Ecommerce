7495626e448ea39eb5d831d26149d8ce156fe8e4
RavenTamayo <108288626+RavenTamayo@users.noreply.github.com>
2022-11-15 18:55:51 +0800

    Changed Tab icon for admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -13,13 +13,19 @@
     <link rel="stylesheet" href="{{ asset('admin/vendors/mdi/css/materialdesignicons.min.css') }}">
     <link rel="stylesheet" href="{{ asset('admin/vendors/base/vendor.bundle.base.css') }}">
     <!-- endinject -->
+
+    {{-- Tab Icon --}}
+    
+
     <!-- plugin css for this page -->
     <link rel="stylesheet" href="{{ asset('admin/vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
     <!-- End plugin css for this page -->
     <!-- inject:css -->
     <link rel="stylesheet" href="{{ asset('admin/css/style.css') }}">
     <!-- endinject -->
-    <link rel="shortcut icon" href="{{ asset('admin/images/favicon.png') }}" />
+    {{-- <link rel="shortcut icon" href="{{ asset('admin/images/favicon.png') }}" /> --}}
+    <link rel="shortcut icon" href="{{ url('https://cdn.discordapp.com/attachments/1017009278445432862/1033281796521070653/kaliksahop_logo.png') }}">
+    <link rel="icon" type="image/png" href="{{ url('https://cdn.discordapp.com/attachments/1017009278445432862/1033281796521070653/kaliksahop_logo.png') }}">
     <!--Bootstrap Icons-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <style>
